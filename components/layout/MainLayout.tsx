481c71ee7c21368d9bead677213791cfd0146a24
EniolaCodes <adebimpeniola@gmail.com>
2026-01-26 11:46:57 +0100

    feat(mainlayout): add mainlayout component

--- components/layout/MainLayout.tsx
+++ components/layout/MainLayout.tsx
@@ -1,7 +1,134 @@
-import React from "react";
+"use client";
+import React, { FC } from "react";
+import Image from "next/image";
+import innerShape from "@/public/assets/images/Rectangle 15.svg";
+import outerShape from "@/public/assets/images/Subtract.svg";
+import github from "@/public/assets/images/github.svg";
+import linkedin from "@/public/assets/images/linkedin.svg";
+import facebook from "@/public/assets/images/facebook.svg";
+import youtube from "@/public/assets/images/youtube.svg";
+import arrow from "@/public/assets/images/arrow.svg";
 
-const MainLayout = () => {
-  return <div>MainLayout</div>;
+interface MainLayoutProps {
+  links: { platform: string; url: string }[];
+  profilePicture?: string;
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+}
+
+const platformImages: Record<string, string> = {
+  GitHub: github,
+  LinkedIn: linkedin,
+  YouTube: youtube,
+  Facebook: facebook,
+};
+
+const MainLayout: FC<MainLayoutProps> = ({
+  links = [],
+  profilePicture,
+  firstName,
+  lastName,
+  email,
+}) => {
+  const validLinks = links.filter(
+    (link) => link.platform && platformImages[link.platform],
+  );
+
+  const boxes = new Array(5).fill(null);
+
+  return (
+    <div className="py-28 px-20 lg:block hidden">
+      <div className="h-157.75 w-76.75 relative">
+        <Image
+          className="absolute top-0 left-0"
+          style={{ width: "90%", height: "auto" }}
+          alt="inner shape"
+          src={innerShape}
+        />
+        <Image
+          src={outerShape}
+          alt="outer"
+          className="w-[16rem] absolute top-3 mx-2.5"
+        />
+        <div>
+          <div className="rounded-full w-28 h-28 content-none bg-dark mx-20 top-16 absolute left-0">
+            {profilePicture && (
+              <Image
+                src={profilePicture}
+                alt="Profile Picture"
+                className="rounded-full"
+                layout="fill"
+              />
+            )}
+          </div>
+          {!firstName && !lastName && (
+            <>
+              <div className="w-40 top-48 mx-[3.6rem] absolute h-5 rounded-full bg-dark" />
+              <div className="w-20 top-56 mx-[6.4rem] absolute h-2 rounded-full bg-dark" />
+            </>
+          )}
+          {firstName && lastName && (
+            <div className="text-center absolute top-48 left-0 right-0 ">
+              <span className="text-black mr-8 text-xl font-semibold">
+                {firstName} {lastName}
+              </span>
+            </div>
+          )}
+          {email && (
+            <div className="text-center absolute top-56 left-0 right-0 ">
+              <span className="text-black mr-8 text-sm">{email}</span>
+            </div>
+          )}
+        </div>
+        <div className="absolute flex gap-3 flex-col top-66 left-0 mx-9">
+          {boxes.map((_, index) => (
+            <div
+              key={index}
+              className={`rounded-md w-52 h-11 flex items-center justify-between pl-2 gap-1 text-white ${
+                validLinks[index] && validLinks[index].platform === "GitHub"
+                  ? "bg-black"
+                  : validLinks[index] &&
+                      validLinks[index].platform === "LinkedIn"
+                    ? "bg-[#2D68FF]"
+                    : validLinks[index] &&
+                        validLinks[index].platform === "YouTube"
+                      ? "bg-red"
+                      : validLinks[index] &&
+                          validLinks[index].platform === "Facebook"
+                        ? "bg-[#4267B2]"
+                        : "bg-dark"
+              }`}
+            >
+              {validLinks[index] ? (
+                <>
+                  <div className="flex gap-1 items-center mx-1">
+                    <Image
+                      src={platformImages[validLinks[index].platform] || ""}
+                      alt={validLinks[index].platform}
+                      width={20}
+                      height={20}
+                    />
+                    <a
+                      href={validLinks[index].url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-white"
+                    >
+                      {validLinks[index].platform}
+                    </a>
+                  </div>
+                  <Image src={arrow} alt="arrow" className="mx-2" />
+                </>
+              ) : (
+                <span className="text-gray-500"></span>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MainLayout;
