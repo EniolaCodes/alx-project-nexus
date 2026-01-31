9231b899a10111bd10d6624e4858ffc328022cb5
EniolaCodes <adebimpeniola@gmail.com>
2026-01-31 14:01:11 +0100

    fix: resolve link overwriting by implementing unique Auto-IDs in Firestore

--- components/preview/Preview.tsx
+++ components/preview/Preview.tsx
@@ -108,7 +108,7 @@ const PreviewPage: FC<PreviewProps> = ({ links, imageUrl, email }) => {
 
   return (
     <div className="min-h-screen w-full bg-gray-100">
-      <div className="relative sm:bg-none bg-[#633CFF] rounded-bl-4xl rounded-br-4xl h-92.5 flex flex-col gap-15 lg:gap-26.5 sm:gap-31.5 z-10">
+      <div className="relative md:bg-[#633CFF] rounded-bl-4xl rounded-br-4xl h-92.5 flex flex-col gap-15 lg:gap-26.5 sm:gap-31.5 z-10">
         <div className="w-full top-0 sm:px-6 sm:py-4">
           <nav className="px-6 py-4 rounded-xl bg-white w-full flex justify-center gap-4 sm:gap-0 sm:justify-between items-center">
             <Link
@@ -126,7 +126,7 @@ const PreviewPage: FC<PreviewProps> = ({ links, imageUrl, email }) => {
           </nav>
         </div>
         <div className="w-full flex justify-center items-center ">
-          <div className="h-fit w-[349px] rounded-3xl bg-white flex flex-col py-12 gap-14 items-center">
+          <div className="h-fit w-87.25 rounded-3xl md:bg-white flex flex-col py-12 gap-8 items-center">
             <div className="flex flex-col gap-6.25 items-center">
               {profileData.imageUrl ? (
                 <Image
@@ -139,7 +139,7 @@ const PreviewPage: FC<PreviewProps> = ({ links, imageUrl, email }) => {
               ) : (
                 <div className="rounded-full w-28 h-28 bg-[#EEEEEE]" />
               )}
-              <div className="flex flex-col gap-3.25 items-center">
+              <div className="flex flex-col items-center">
                 {profileData.firstName && profileData.lastName && (
                   <div className="text-[#333333] text-[32px] font-bold mt-2">
                     {profileData.firstName} {profileData.lastName}
@@ -147,13 +147,13 @@ const PreviewPage: FC<PreviewProps> = ({ links, imageUrl, email }) => {
                 )}
 
                 {profileData.email && (
-                  <div className="text-[#737373] text-[16px] mt-2">
+                  <div className="text-[#737373] text-[16px]">
                     {profileData.email}
                   </div>
                 )}
               </div>
             </div>
-            <div className="flex gap-5 w-full sm:px-14 flex-col top-50 left-0 mx-9">
+            <div className="flex gap-5 w-full sm:px-14 flex-col left-0 mx-9">
               {linksData.map((link, index) => (
                 <div
                   key={index}
